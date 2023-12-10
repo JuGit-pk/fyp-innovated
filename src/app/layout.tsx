5751a0f6854e279e611f696ea68d492a7a229a69
Muhammad Junaid Iqbal <junaidi2k1@gmail.com>
2023-12-11 01:27:07 +0500

    - feat(ui): add fonts and set the global common styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
+import "@/styles/globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "../styles/globals.css";
+// import { Inter } from "next/font/google";
+import { dm_sans, inter } from "@/lib/fonts";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "InnovatED",
@@ -16,8 +17,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${inter.variable} ${dm_sans.variable}`}
+      suppressHydrationWarning
+    >
+      {/* <Header stargazers_count={stargazers_count} /> */}
       <body className={inter.className}>{children}</body>
+      {/* <Footer /> */}
     </html>
   );
 }
