c2018a1ec75f5b3475a2c950349c4d77786d0cc0
Muhammad Junaid Iqbal <junaid@codainer.com>
2024-04-14 02:09:32 +0500

    - fix(ui): some ui for the document layout

--- src/app/(main)/documents/[id]/layout.tsx
+++ src/app/(main)/documents/[id]/layout.tsx
@@ -1,5 +1,6 @@
 import ChatContainer from "@/components/chat/chat-container";
 import SideNav from "@/components/layout/document/sidenav";
+import { Separator } from "@/components/ui/separator";
 import React from "react";
 
 type IProps = {
@@ -11,10 +12,13 @@ const DocumentIdLayout: React.FC<IProps> = ({ children, params }) => {
   return (
     <div className="flex max-h-[calc(100vh-81px)] min-h-[calc(100vh-81px)] h-full">
       <SideNav params={params} />
-      <div className="flex-1 w-2/3 max-h-[calc(100vh-81px)] min-h-[calc(100vh-81px)]">
+      <div className="flex-1 w-2/3 max-h-[calc(100vh-81px)] min-h-[calc(100vh-81px)] overflow-y-auto">
         {children}
       </div>
       {/* <div className="w-px bg-black" /> */}
+      <div>
+        <Separator orientation="vertical" className="h-full" />
+      </div>
       <div className="w-1/3">
         <ChatContainer params={params} />
       </div>
